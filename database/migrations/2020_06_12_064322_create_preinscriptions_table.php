6692d2e08127a0ceead0be04a5d9636ebe85743f
tekyid237 <itekeudoyepmo@gmail.com>
2020-06-12 08:33:45 +0100

    "make Preinscription model"

---
+++ database/migrations/2020_06_12_064322_create_preinscriptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreinscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preinscriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('lastname');
+            $table->char('gender', 1);
+            $table->date('birth-date');
+            $table->string('branch');
+            $table->string('level');
+            $table->text('sup-infos')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preinscriptions');
+    }
+}
